703e6ce5af2cecc3fe373ee5733d4123960897f2
Ifcodes <ifeco133@gmail.com>
2022-08-18 18:05:35 +0100

    fix: fixed post request

--- src/Page/Homepage/homepageStyles.tsx
+++ src/Page/Homepage/homepageStyles.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const HomePageWrapper = styled.div`
+  @import url("https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap");
+
   width: 90%;
   margin: 4rem auto;
+  font-family: "Roboto", sans-serif;
 `;
--- src/Styles/GlobalStyles.ts
+++ src/Styles/GlobalStyles.ts
@@ -6,7 +6,7 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     margin: 0;
     font-size: 1rem;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Roboto', sans-serif,
   }
 
   *{
